a21a411ffa7532ac5ca3641df04607f959450e50
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2021-11-19 01:27:43 -0300

    Propiedades y constructor agregados

--- LaTienda.Dominio/Stock.cs
+++ LaTienda.Dominio/Stock.cs
@@ -8,6 +8,17 @@ namespace LaTienda.Dominio
 {
     public class Stock
     {
+        public Producto Producto { get; private set; }
+        public Talle Talle { get; private set; }
+        public Color Color { get; private set; }
+        public int CantidadEnStock { get; private set; }
 
+        public Stock(Producto producto, Talle talle, Color color, int cantidadEnStock)
+        {
+            Producto = producto;
+            Talle = talle;
+            Color = color;
+            CantidadEnStock = cantidadEnStock;
+        }
     }
 }
