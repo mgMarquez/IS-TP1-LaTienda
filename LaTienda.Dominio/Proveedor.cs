85f7c0d8ca0ebe748a497b545249888219b99210
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2021-12-11 21:01:38 -0300

    Agregada clase proveedor

---
+++ LaTienda.Dominio/Proveedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Dominio
+{
+    public class Proveedor
+    {
+        public int ProveedorID { get; set; }
+        public string Descripcion { get; set; }
+
+        public Proveedor(int proveedorID, string descripcion)
+        {
+            ProveedorID = proveedorID;
+            Descripcion = descripcion;
+        }
+    }
+}
