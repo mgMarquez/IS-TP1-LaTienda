fd6cdf66363b04a668e37ab432cf19c70822667a
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2021-12-16 17:21:34 -0300

    Agregada propiedad Rubro

--- LaTienda.Dominio/Producto.cs
+++ LaTienda.Dominio/Producto.cs
@@ -11,20 +11,22 @@ namespace LaTienda.Dominio
         public int ProductoID { get; set; }
         public string Descripcion { get; set; }
         public double Costo { get; set; }
+        public double PorcentajeDeIva { get; set; }
         public double MargenDeGanancia { get; set; }
         public double NetoGravado => Costo + Costo * MargenDeGanancia;
         public double IVA => NetoGravado * PorcentajeDeIva;
         public double PrecioDeVenta => NetoGravado + IVA;
 
         public Marca Marca { get; set; }
+        public Rubro Rubro { get; set; }
         public List<Stock> DetalleDeStock { get; private set; }
-        public double PorcentajeDeIva { get; set; }
 
         public Producto(string descripcion, 
-            Marca marca, double costo, double porcentajeIVA, double margenDeGanancia)
+            Marca marca, Rubro rubro, double costo, double porcentajeIVA, double margenDeGanancia)
         {
             Descripcion = descripcion;
             Marca = marca;
+            Rubro = rubro;
             Costo = costo;
             PorcentajeDeIva = porcentajeIVA;
             MargenDeGanancia = margenDeGanancia;
