4eade8cb28a0363c8457ae1d369f4a8ab4738b3a
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-02-24 19:47:42 -0300

    Nuevos metodos en las interfaces

--- LaTienda.Presentador/Vista/IRegistrarVentaVista.cs
+++ LaTienda.Presentador/Vista/IRegistrarVentaVista.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LaTienda.Dominio;
 
 namespace LaTienda.Presentador
 {
     public interface IRegistrarVentaVista
     {
+        void MostrarProducto(Producto productoActual);
+        void MostrarDetalleDeVenta(List<LineaDeVenta> detalleVenta);
     }
 }
