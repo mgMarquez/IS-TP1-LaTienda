13b7cf3daa6db5bb2780e178f9d107fb93af56c9
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-02-26 18:00:32 -0300

    Interfaz generica de repositorio

---
+++ LaTienda.Infraestructura.Datos/Repositorio/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Infraestructura.Datos.Repositorio
+{
+    interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Create(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(int id);
+        void Update(TEntity entity);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> filtro);
+    }
+}
