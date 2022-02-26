c6e594dd33027cf583099e59d770645df5bbd4d4
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-02-26 18:20:59 -0300

    patron unidad de trabajo interface e implementación

---
+++ LaTienda.Infraestructura.Datos/UoW/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using LaTienda.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Infraestructura.Datos
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Producto> ProductoRepository { get; }
+        IGenericRepository<Marca> MarcaRepository { get; }
+        IGenericRepository<Rubro> RubroRepository { get; }
+        void Save();
+    }
+}
---
+++ LaTienda.Infraestructura.Datos/UoW/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaTienda.Dominio;
+
+namespace LaTienda.Infraestructura.Datos
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TiendaContext _context;
+        private IGenericRepository<Producto> _productoRepository;
+        private IGenericRepository<Marca> _marcaRepository;
+        private IGenericRepository<Rubro> _rubroRepository;
+
+        public UnitOfWork(TiendaContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Producto> ProductoRepository
+        {
+            get
+            {
+                if (_productoRepository == null)
+                {
+                    _productoRepository = new GenericRepository<Producto>(_context);
+                }
+                return _productoRepository;
+            }
+        }
+
+        public IGenericRepository<Marca> MarcaRepository
+        {
+            get
+            {
+                if (_marcaRepository == null)
+                {
+                    _marcaRepository = new GenericRepository<Marca>(_context);
+                }
+                return _marcaRepository;
+            }
+        }
+
+        public IGenericRepository<Rubro> RubroRepository
+        {
+            get
+            {
+                if (_rubroRepository == null)
+                {
+                    _rubroRepository = new GenericRepository<Rubro>(_context);
+                }
+                return _rubroRepository;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
