135063d2068528182d0fb41d68522cac0dc34e75
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-02-26 17:47:22 -0300

    nuevas propiedades para mostrar en la tabla

--- LaTienda.Dominio/Entidades/LineaDeVenta.cs
+++ LaTienda.Dominio/Entidades/LineaDeVenta.cs
@@ -13,6 +13,12 @@ namespace LaTienda.Dominio
         public Talle Talle { get; private set; }
         public int Cantidad { get; set; }
 
+        public int CodigoProducto => Producto.Codigo;
+        public string DescripcionProducto => Producto.Descripcion;
+        public string MarcaProducto => Producto.Marca.Descripcion;
+        public double PrecioProducto => Producto.PrecioDeVenta;
+        public string DescripcionColor => Color.Descripcion;
+        public string DescripcionTalle => Talle.Descripcion;
         public double SubTotal => Producto.PrecioDeVenta * Cantidad;
         public double NetoGravadoTotal => Producto.NetoGravado * Cantidad;
         public double IVATotal => Producto.IVA * Cantidad;
