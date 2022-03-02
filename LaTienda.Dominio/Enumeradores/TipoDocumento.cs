5d2084ad7efeb24b7c8f803ef465513c1052a0a9
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-02 17:13:20 -0300

    enumerador de tipo comprobante

---
+++ LaTienda.Dominio/Enumeradores/TipoDocumento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Dominio
+{
+    public enum TipoDocumento
+    {
+        CUIT = 80,
+        CUIL = 86,
+        DNI = 96,
+        Pasaporte = 94
+    }
+}
