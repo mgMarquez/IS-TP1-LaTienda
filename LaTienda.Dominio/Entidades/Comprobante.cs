84865709589e59857a5af8bf6bb58ebdd2d64004
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-02 18:36:19 -0300

    Nuevas propiedades en comprobante

--- LaTienda.Dominio/Entidades/Comprobante.cs
+++ LaTienda.Dominio/Entidades/Comprobante.cs
@@ -10,8 +10,17 @@ namespace LaTienda.Dominio
     {
         public int ComprobanteID { get; set; }
         public int NumeroDeComprobante { get; set; }
-        public int PuntoDeVenta { get; set; }
+        public TipoDocumento DocumentoTipoCliente { get; set; }
+        public long NumeroDocumentoCliente { get; set; }
         public DateTime Fecha { get; set; }
+        public double ImporteTotal { get; set; } // Importe total
+        public double ImpTotConc { get; set; } // Importe neto no gravado
+        public double ImpNeto { get; set; } // Importe neto gravado
+        public double ImpOpEx { get; set; } // Importe exento
+        public double ImpIVA { get; set; } // Suma de los importes del array de IVA
+        public double ImpTrib { get; set; } // Suma de los importes del array de tributos
+
+        public int PuntoDeVenta { get; set; }
         public TipoComprobante TipoComprobante { get; set; }
 
         public void AsignarNumeroComprobante(int ultimoNumeroComprobante)
@@ -19,6 +28,6 @@ namespace LaTienda.Dominio
             NumeroDeComprobante = ultimoNumeroComprobante + 1;
         }
 
-        public string FechaStringYYYYMMDD => Fecha.ToString("yyyyMMdd");
+        public string FechaString => Fecha.ToString("yyyyMMdd");
     }
 }
