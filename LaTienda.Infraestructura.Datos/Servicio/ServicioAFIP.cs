378c3c6c74f5c869be9774af6ca1dfbe76f20b7c
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-02 18:38:02 -0300

    Servicio afip terminado - falta probar

--- LaTienda.Infraestructura.Datos/Servicio/ServicioAFIP.cs
+++ LaTienda.Infraestructura.Datos/Servicio/ServicioAFIP.cs
@@ -11,18 +11,15 @@ namespace LaTienda.Infraestructura.Datos.Servicio
     public static class ServicioAFIP
     {
 
-        public static void SolicitarAutorizacionComprobante(Venta venta, Comprobante comprobante)
+        public static void SolicitarAutorizacionComprobante(Comprobante comprobante)
         {
             var auth = ObtenerAutorizacion();
+            int puntoVenta = comprobante.PuntoDeVenta;
+            int comprobanteTipo = (int)comprobante.TipoComprobante;
 
             var servicioAfip = new WSAFIP.ServiceSoapClient();
-
-            int puntoVenta = new PuntoDeVenta().NumeroPDV; //cambiar
-            int comprobanteTipo = (int) new Comprobante().TipoComprobante; //cambiar
-
             var ultimoAutorizado = servicioAfip.FECompUltimoAutorizado(auth, puntoVenta, comprobanteTipo);
-            var ultimoNumCompAut = ultimoAutorizado.CbteNro;
-
+            int ultimoNumCompAut = ultimoAutorizado.CbteNro; // obtenemos ultimo numero de comprobante
             comprobante.AsignarNumeroComprobante(ultimoNumCompAut);
 
             // Informacíón del comprobante. Cabezera y detalle del comprobante
@@ -37,12 +34,14 @@ namespace LaTienda.Infraestructura.Datos.Servicio
             // TODO: continuar con lo que sigue de la autorización
         }
 
+        #region Autorización y autenticación con web service cliente
         private static WSAFIP.FEAuthRequest ObtenerAutorizacion()
         {
             // web service de autorización y autenticación
             var servicioAutorizacion = new WSAA.LoginServiceClient();
             // solicitud de autorización al servicio externo
-            var certificadoAutorizacion = servicioAutorizacion.SolicitarAutorizacion("C116B5BE-3AE2-4C89-B1C7-424A4A842D48");
+            var certificadoAutorizacion = servicioAutorizacion
+                .SolicitarAutorizacion("C116B5BE-3AE2-4C89-B1C7-424A4A842D48");
             // adapta el certificado de autorización
             var auth = new WSAFIP.FEAuthRequest()
             {
@@ -52,7 +51,10 @@ namespace LaTienda.Infraestructura.Datos.Servicio
             };
             return auth;
         }
+        #endregion
 
+
+        #region Información del comprobante - cabecera y detalle
         private static FECAECabRequest CrearCabeceraDeComprobante(Comprobante comprobante)
         {
             return new FECAECabRequest()
@@ -69,20 +71,23 @@ namespace LaTienda.Infraestructura.Datos.Servicio
             {
                 new FECAEDetRequest()
                 {
-                    Concepto = 1, // 1 Productos
-                    DocTipo = comprobante.NumeroDeComprobante,
-                    DocNro = comprobante.NumeroDeComprobante,
-                    CbteFch = comprobante.FechaStringYYYYMMDD,
-                    ImpTotal = 0, // COMPLETAR
-                    ImpTotConc = 0,
-                    ImpNeto = 0,
-                    ImpOpEx = 0,
-                    ImpIVA = 0,
-                    ImpTrib = 0,
-                    MonId = "PES",
+                    Concepto = 1, // Concepto del Comprobante - 1 Productos
+                    DocTipo = (int)comprobante.DocumentoTipoCliente, // Código de documento identificatorio del comprador
+                    DocNro = comprobante.NumeroDocumentoCliente, // Nro. De identificación del comprador
+                    CbteDesde = 1,
+                    CbteHasta = 1,
+                    CbteFch = comprobante.FechaString, // formato yyyyMMdd
+                    ImpTotal = comprobante.ImporteTotal,
+                    ImpTotConc = comprobante.ImpTotConc,
+                    ImpNeto = comprobante.ImpNeto,
+                    ImpOpEx = comprobante.ImpOpEx,
+                    ImpIVA = comprobante.ImpIVA,
+                    ImpTrib = comprobante.ImpTrib,
+                    MonId = "PES", // Código de moneda del comprobante - PES para pesos argentinos
                     MonCotiz = 1, // para pesos argentinos es 1
                 }
             };
         }
+        #endregion
     }
 }
