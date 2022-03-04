73c47ed84cb3cfe64b59400dc61530dbe3383750
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-04 00:36:42 -0300

    nuevo controlador para gestionar cliente

---
+++ LaTienda.Presentador/Presentadores/ControladorGestionarCliente.cs
@@ -0,0 +1,21 @@
+﻿using LaTienda.Infraestructura.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Presentador
+{
+    public class ControladorGestionarCliente
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGestionarClienteVista _vista;
+
+        public ControladorGestionarCliente(IUnitOfWork unitOfWork, IGestionarClienteVista vista)
+        {
+            _unitOfWork = unitOfWork;
+            _vista = vista;
+        }
+    }
+}
