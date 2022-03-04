636e94ebe968181736b183c90d58b5933d267ed1
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-03 23:48:12 -0300

    creada vista gestionar cliente

---
+++ LaTienda.Presentador/Vista/GestionarClienteVista.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace LaTienda.Presentador.Vista
+{
+    partial class GestionarClienteVista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelGestionarCliente = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtAceptar = new System.Windows.Forms.Button();
+            this.BtCancelar = new System.Windows.Forms.Button();
+            this.BtSalir = new System.Windows.Forms.Button();
+            this.groupBoxCliente = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.TbNroDocumento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TbRazonSocial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CbTipoDocumento = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TbDomicilio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CbCondicionTributaria = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.groupBoxCliente.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelGestionarCliente
+            // 
+            this.labelGestionarCliente.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelGestionarCliente.AutoSize = true;
+            this.labelGestionarCliente.Location = new System.Drawing.Point(227, 28);
+            this.labelGestionarCliente.Name = "labelGestionarCliente";
+            this.labelGestionarCliente.Size = new System.Drawing.Size(146, 20);
+            this.labelGestionarCliente.TabIndex = 0;
+            this.labelGestionarCliente.Text = "Gestionar cliente";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.labelGestionarCliente, 0, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 76);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel4.ColumnCount = 5;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.BtAceptar, 4, 0);
+            this.tableLayoutPanel4.Controls.Add(this.BtCancelar, 3, 0);
+            this.tableLayoutPanel4.Controls.Add(this.BtSalir, 1, 0);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(12, 384);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(600, 45);
+            this.tableLayoutPanel4.TabIndex = 5;
+            // 
+            // BtAceptar
+            // 
+            this.BtAceptar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtAceptar.Location = new System.Drawing.Point(504, 11);
+            this.BtAceptar.Name = "BtAceptar";
+            this.BtAceptar.Size = new System.Drawing.Size(75, 23);
+            this.BtAceptar.TabIndex = 3;
+            this.BtAceptar.Text = "Aceptar";
+            this.BtAceptar.UseVisualStyleBackColor = true;
+            // 
+            // BtCancelar
+            // 
+            this.BtCancelar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtCancelar.Location = new System.Drawing.Point(388, 11);
+            this.BtCancelar.Name = "BtCancelar";
+            this.BtCancelar.Size = new System.Drawing.Size(75, 23);
+            this.BtCancelar.TabIndex = 2;
+            this.BtCancelar.Text = "Cancelar";
+            this.BtCancelar.UseVisualStyleBackColor = true;
+            // 
+            // BtSalir
+            // 
+            this.BtSalir.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.BtSalir.Location = new System.Drawing.Point(23, 11);
+            this.BtSalir.Name = "BtSalir";
+            this.BtSalir.Size = new System.Drawing.Size(75, 23);
+            this.BtSalir.TabIndex = 0;
+            this.BtSalir.Text = "Salir";
+            this.BtSalir.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxCliente
+            // 
+            this.groupBoxCliente.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxCliente.Controls.Add(this.tableLayoutPanel2);
+            this.groupBoxCliente.Location = new System.Drawing.Point(12, 94);
+            this.groupBoxCliente.Name = "groupBoxCliente";
+            this.groupBoxCliente.Size = new System.Drawing.Size(600, 284);
+            this.groupBoxCliente.TabIndex = 6;
+            this.groupBoxCliente.TabStop = false;
+            this.groupBoxCliente.Text = "Cliente";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.87755F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.95238F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.57823F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.93197F));
+            this.tableLayoutPanel2.Controls.Add(this.TbNroDocumento, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.CbTipoDocumento, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.TbRazonSocial, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.TbDomicilio, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.CbCondicionTributaria, 1, 2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 19);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(588, 178);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // TbNroDocumento
+            // 
+            this.TbNroDocumento.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbNroDocumento.Location = new System.Drawing.Point(113, 19);
+            this.TbNroDocumento.Name = "TbNroDocumento";
+            this.TbNroDocumento.Size = new System.Drawing.Size(175, 20);
+            this.TbNroDocumento.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nro documento";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo de documento";
+            // 
+            // TbRazonSocial
+            // 
+            this.TbRazonSocial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbRazonSocial.Location = new System.Drawing.Point(113, 78);
+            this.TbRazonSocial.Name = "TbRazonSocial";
+            this.TbRazonSocial.Size = new System.Drawing.Size(175, 20);
+            this.TbRazonSocial.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Razon social";
+            // 
+            // CbTipoDocumento
+            // 
+            this.CbTipoDocumento.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.CbTipoDocumento.FormattingEnabled = true;
+            this.CbTipoDocumento.Location = new System.Drawing.Point(414, 19);
+            this.CbTipoDocumento.Name = "CbTipoDocumento";
+            this.CbTipoDocumento.Size = new System.Drawing.Size(171, 21);
+            this.CbTipoDocumento.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Domicilio";
+            // 
+            // TbDomicilio
+            // 
+            this.TbDomicilio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbDomicilio.Location = new System.Drawing.Point(414, 78);
+            this.TbDomicilio.Name = "TbDomicilio";
+            this.TbDomicilio.Size = new System.Drawing.Size(171, 20);
+            this.TbDomicilio.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Condición tributaria";
+            // 
+            // CbCondicionTributaria
+            // 
+            this.CbCondicionTributaria.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.CbCondicionTributaria.FormattingEnabled = true;
+            this.CbCondicionTributaria.Location = new System.Drawing.Point(113, 137);
+            this.CbCondicionTributaria.Name = "CbCondicionTributaria";
+            this.CbCondicionTributaria.Size = new System.Drawing.Size(175, 21);
+            this.CbCondicionTributaria.TabIndex = 2;
+            // 
+            // GestionarClienteVista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.groupBoxCliente);
+            this.Controls.Add(this.tableLayoutPanel4);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "GestionarClienteVista";
+            this.Text = "La tienda - Gestionar cliente";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.groupBoxCliente.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelGestionarCliente;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button BtAceptar;
+        private System.Windows.Forms.Button BtCancelar;
+        private System.Windows.Forms.Button BtSalir;
+        private System.Windows.Forms.GroupBox groupBoxCliente;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox TbNroDocumento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CbTipoDocumento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TbRazonSocial;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TbDomicilio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox CbCondicionTributaria;
+    }
+}
---
+++ LaTienda.Presentador/Vista/GestionarClienteVista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaTienda.Presentador.Vista
+{
+    public partial class GestionarClienteVista : Form
+    {
+        public GestionarClienteVista()
+        {
+            InitializeComponent();
+        }
+    }
+}
