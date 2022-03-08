1704a820cce3dfe4f8418f96d35d3c963c224406
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-08 18:23:34 -0300

    nueva interface para la vista de producto

---
+++ LaTienda.Presentador/Contratos/IProductoVista.cs
@@ -0,0 +1,19 @@
+﻿using LaTienda.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Presentador
+{
+    public interface IProductoVista
+    {
+        void MostrarProducto(Producto producto);
+        void MostrarMarcas(List<Marca> marcas);
+        void MostrarRubros(List<Rubro> rubros);
+        void VistaCrear();
+        void VistaEliminar();
+        void VistaModificar();
+    }
+}
