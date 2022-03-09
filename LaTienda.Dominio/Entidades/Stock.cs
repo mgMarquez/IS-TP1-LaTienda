9dd104d979887c8d6fbdaf579a1fb9a7f2972e15
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-08 23:50:05 -0300

    arreglado metodos para poder modificar desde la vista

--- LaTienda.Dominio/Entidades/Stock.cs
+++ LaTienda.Dominio/Entidades/Stock.cs
@@ -9,25 +9,15 @@ namespace LaTienda.Dominio
     public class Stock
     {
         public int StockID { get; set; }
-        public virtual Talle Talle { get; private set; }
-        public virtual Color Color { get; private set; }
-        public int CantidadEnStock { get; private set; }
+        public virtual Talle Talle { get; set; }
+        public virtual Color Color { get; set; }
+        public int CantidadEnStock { get; set; }
 
         public int TalleID { get; set; }
         public int ColorID { get; set; }
         public int ProductoID { get; set; }
         public virtual Producto Producto { get; set; }
 
-        public Stock()
-        {
 
-        }
-
-        public Stock(Talle talle, Color color, int cantidadEnStock)
-        {
-            Talle = talle;
-            Color = color;
-            CantidadEnStock = cantidadEnStock;
-        }
     }
 }
