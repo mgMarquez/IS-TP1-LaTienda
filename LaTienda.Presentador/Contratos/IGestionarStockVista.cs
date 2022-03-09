a0d5e5b12ff300cb67a4fb490f2867a1c45c4201
Mauricio Marquez <mauriciomarquez@alu.frt.utn.edu.ar>
2022-03-08 23:51:45 -0300

    Interfaces de gestionar stock vista y stock vista

---
+++ LaTienda.Presentador/Contratos/IGestionarStockVista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaTienda.Dominio;
+
+namespace LaTienda.Presentador
+{
+    public interface IGestionarStockVista
+    {
+        void MostrarListaStocks(List<Stock> stocks);
+    }
+}
---
+++ LaTienda.Presentador/Contratos/IStockVista.cs
@@ -0,0 +1,20 @@
+﻿using LaTienda.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTienda.Presentador
+{
+    public interface IStockVista
+    {
+        void MostrarProductos(List<Producto> productos);
+        void MostrarTalles(List<Talle> talles);
+        void MostrarColores(List<Color> colores);
+        void MostrarStock(Stock stock);
+        void VistaCrear();
+        void VistaEliminar();
+        void VistaModificar();
+    }
+}
